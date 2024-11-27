7ef2c2087f56e4df06c1faeb5d6355a07849b25d
Anish Sarkar <anish.sarkar@codeclouds.in>
2024-11-27 15:32:41 +0530

    Final Biild.

---
+++ app/dashboard/ui/fonts.tsx
@@ -0,0 +1,15 @@
+import { Inter,Roboto_Mono, Lusitana } from 'next/font/google';
+ 
+
+export const inter = Inter({ subsets: ['latin'] }); 
+
+export const lusitana = Lusitana({ weight:'400' ,subsets: ['latin']
+});
+
+export const roboto_mono = Roboto_Mono({
+
+subsets: ['latin'],
+
+weight: '400'
+
+});
